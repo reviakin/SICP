7a098eaf1f822d032ca7c90def181f2760fa86dd
reviakin <reviakindi@gmail.com>
2021-03-30 22:55:11 +0200

    Resolved 2.7

---
+++ 2.7.ts
@@ -0,0 +1,36 @@
+/**
+ * @module data abstraction.
+ * 2.1. Data abstraction
+ * 2.7.
+ * Программа Лизы неполна, поскольку она не
+ * определила, как реализуется абстракция интервала.
+ * Вот определение конструктора интервала:
+ * (define (make-interval a b) (cons a b))
+ * Завершите реализацию, определив селекторы
+ * upper-bound и lower-bound.
+ */
+
+
+/** */
+const makePair = <T>(a: T, b: T): [T, T] => [a, b];
+
+const getHead = <T>(p:[T,T]): T => p[0];
+
+const getTail = <T>(p:[T, T]) :T => p[1];
+
+/** */
+const makeInterval = (a: number, b: number) => 
+    makePair<number>(a, b);
+
+const getUpperBound = (interval:[number, number]) => 
+    getHead<number>(interval);
+
+const getLoweBound = (interval:[number, number]) => 
+    getTail<number>(interval);
+
+/** */
+export {
+    makeInterval, 
+    getUpperBound, 
+    getLoweBound,
+}
