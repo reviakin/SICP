a05b12439e0216e25b92ff53a6c42b139e15bc6e
reviakin <reviakindi@gmail.com>
2021-04-29 01:10:39 +0200

    Added function monitor

---
+++ function_monitor.ts
@@ -0,0 +1,18 @@
+/**
+ * @module 3.2.
+ * @param {Function} f 
+ * @returns {Function}
+ */
+function make_monitor(f:Function){
+    let inner_counter = 0;
+
+    return (
+        param : 'how many?' | 'reset count' | any
+    ) => {
+        if (param === 'how many?') return inner_counter;
+        if (param === 'reset count') return inner_counter = 0;
+        
+        inner_counter++
+        return f(param)
+    }
+}
