9c5b648e558caeebaecce8bb48a05c5a6df97541
reviakin <reviakindi@gmail.com>
2021-04-29 01:33:49 +0200

    Added 3.2.

--- accaunt.ts
+++ accaunt.ts
@@ -14,3 +14,33 @@ function make_account(balance: number) {
     }
     return dispatch;
 }
+
+/**
+ * @module 3.3.
+ * @param {number} balance 
+ * @returns {Function}
+ */
+function make_account_with_password(balance: number, password: string) {
+
+    function password_required(operation:Function){
+        return (_password:string, amount:number) => {
+            if (_password !== password) throw 'invalid password'
+            return operation(amount)
+        }
+    }
+    
+    function deposit(amount: number) {
+        return (balance += amount);
+    }
+    function withdraw(amount: number) {
+        if (balance < amount) throw 'not enough money';
+        return (balance -= amount);
+    }
+
+    function dispatch(operator: string) {
+        if (operator === 'deposit') return password_required(deposit);
+        if (operator === 'withdraw') return password_required(withdraw);
+        throw 'unknown operator';
+    }
+    return dispatch;
+}
