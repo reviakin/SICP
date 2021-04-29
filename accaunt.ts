dd52035b4c0bf1ea19a27eed60cf239db28dcaee
reviakin <reviakindi@gmail.com>
2021-04-29 02:08:54 +0200

    Addded 3.4.

--- accaunt.ts
+++ accaunt.ts
@@ -1,3 +1,5 @@
+import { make_monitor } from './function_monitor';
+
 function make_account(balance: number) {
     function deposit(amount: number) {
         return (balance += amount);
@@ -17,18 +19,17 @@ function make_account(balance: number) {
 
 /**
  * @module 3.3.
- * @param {number} balance 
+ * @param {number} balance
  * @returns {Function}
  */
 function make_account_with_password(balance: number, password: string) {
-
-    function password_required(operation:Function){
-        return (_password:string, amount:number) => {
-            if (_password !== password) throw 'invalid password'
-            return operation(amount)
-        }
+    function password_required(operation: Function) {
+        return (_password: string, amount: number) => {
+            if (_password !== password) throw 'invalid password';
+            return operation(amount);
+        };
     }
-    
+
     function deposit(amount: number) {
         return (balance += amount);
     }
@@ -44,3 +45,66 @@ function make_account_with_password(balance: number, password: string) {
     }
     return dispatch;
 }
+
+/**
+ * @module 3.4.
+ * @param {number} balance 
+ * @param {string} password 
+ * @returns {Function}
+ */
+function make_account_with_password_with_guard(
+    balance: number,
+    password: string
+) {
+    function call_the_cops() {
+        console.log('call_the_cops');
+    }
+
+    function get_error(message: string) {
+        throw message;
+    }
+
+    function get_password_error() {
+        return get_error('invalid password');
+    }
+
+    const PASSWORD_ERROR_MAX_COUNT = 7;
+
+    let get_monitored_password_error = make_monitor(get_password_error);
+
+    function password_required(operation: Function) {
+        return (_password: string, amount: number) => {
+            if (_password !== password) {
+                if (
+                    get_monitored_password_error('how many?') <
+                    PASSWORD_ERROR_MAX_COUNT
+                ) {
+                    return get_monitored_password_error();
+                } else return call_the_cops();
+            }
+            get_monitored_password_error('reset count');
+            return operation(amount);
+        };
+    }
+
+    function deposit(amount: number) {
+        return (balance += amount);
+    }
+    function withdraw(amount: number) {
+        if (balance < amount) return get_error('not enough money');
+        return (balance -= amount);
+    }
+
+    function dispatch(operator: string) {
+        if (operator === 'deposit') return password_required(deposit);
+        if (operator === 'withdraw') return password_required(withdraw);
+        return get_error('unknown operator');
+    }
+    return dispatch;
+}
+
+export {
+    make_account,
+    make_account_with_password,
+    make_account_with_password_with_guard,
+};
