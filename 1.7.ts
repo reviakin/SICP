14a2d5acca0f6022552324adc5944dbcef75878a
reviakin <reviakindi@gmail.com>
2021-07-19 21:43:58 +0200

    Finished 1.7

--- 1.7.ts
+++ 1.7.ts
@@ -13,12 +13,23 @@ function average(x: number, y: number): number {
 function new_if<T>(predicate: boolean, then_clause: T, else_clause: T): T {
   return predicate ? then_clause : else_clause;
 }
-
+let j = 0;
 function sqrt_iter(guess: number, x: number): number {
-  //  if (good_enough(guess, x)) {
-  //    return guess;
-  //  }
-  //  return sqrt_iter(improve(guess, x), x);
-
-  return new_if<number>(good_enough(guess, x), guess, improve(guess, x));
+  j++;
+  return good_enough(guess, x) ? guess : sqrt_iter(improve(guess, x), x);
 }
+
+function good_enough1(guess1: number, guess2: number): boolean {
+  return Math.abs(guess1 - guess2) < 0.001;
+}
+let i = 0;
+function sqrt_iter1(guess: number, x: number, old_guess = 0): number {
+  i++;
+  return good_enough1(guess, old_guess)
+    ? guess
+    : sqrt_iter1(improve(guess, x), x, guess);
+}
+let testsq = 12312312312312313;
+console.log(sqrt_iter1(1, testsq));
+console.log(sqrt_iter(1, testsq));
+console.log({ i, j });
