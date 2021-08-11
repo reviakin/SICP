f66dd7b25380104b58d032a5134656aaa38a5da4
reviakin <reviakindi@gmail.com>
2021-08-11 16:54:49 +0200

    Fineshed 1.16

--- 1.16.py
+++ 1.16.py
@@ -1,10 +1,11 @@
 def exp_r(a, n):
     print('exp_r_step')
-    if n == 0: return 1;
+    if n == 0: return 1
 
-    return exp_r(a * a, n -1)
+    
+    return a * exp_r(a, n -1)
 
-exp_r(12, 12)
+print(exp_r(12, 12))
 
 def even(a):
     if a % 2 == 0: return True
@@ -18,9 +19,35 @@ def exp_r_f(a, n):
     def fast_exp(b, m):
         print('exp_r_f_step')
         if m == 0: return 1
-        if even(n): return fast_exp(square(b), m/2)
-        return fast_exp(b * b, m -1)
+        if even(m): return square(fast_exp(b, m / 2))
+        return b * fast_exp(b, m -1)
 
     return fast_exp(a, n)
 
-exp_r_f(12,12)
+print(exp_r_f(12,12))
+
+def exp_i(a,n):
+    
+    def iterative_exp(m, product):
+        print('exp_i_step')
+        if m == 0:
+            return product
+        return iterative_exp(m - 1, product * a)
+
+    return iterative_exp(n, 1);
+
+print(exp_i(12, 12))
+
+def exp_i_f(a,n):
+
+    def fast_iterative_exp(a, b, n):
+        print('exp_i_f_step')
+        if n == 0: return a;
+
+        if even(n):
+            return fast_iterative_exp(a, square(b), n/2)
+        return fast_iterative_exp(a * b, b, n - 1)
+
+    return fast_iterative_exp(1, a, n)
+
+print(exp_i_f(12,12))
